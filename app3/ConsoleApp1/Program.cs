86c2f4e2dfc8def3782a70f389dbb18bd4a73241
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-04 16:01:56 +0200

    added solution for Arabic to roman numbers

--- app3/ConsoleApp1/Program.cs
+++ app3/ConsoleApp1/Program.cs
@@ -10,8 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
 
-        {string today = "Thursday";
-    string weekPeriod = today == "Saturday" || today == "Sunday" ? "weekend" : "workday";
+        {
+            string inputData=Console.ReadLine();
+            int Numar =Convert.ToInt32(inputData);
+            
+            if(Numar == 100)
+            {
+                Console.WriteLine("C");
+            }
+            else
+            {
+                string[] digits = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+                string[] dozens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+                int zeci=Numar/10;
+                int unitati=Numar%10;
+                string NumarRoman = (dozens[zeci]+digits[unitati]);
+                Console.WriteLine(NumarRoman);
+            }
+
         }
     }
 }
