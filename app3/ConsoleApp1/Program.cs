3bb73af44bedadb6c3de58523c30b53a311ee3bf
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-04 15:47:48 +0200

    Added new app

---
+++ app3/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {string today = "Thursday";
+    string weekPeriod = today == "Saturday" || today == "Sunday" ? "weekend" : "workday";
+        }
+    }
+}
