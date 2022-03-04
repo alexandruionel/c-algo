5e4a435f25adbb19bebd19e2ddbff6405443e0d3
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-04 16:21:52 +0200

    Added new ecxercise

---
+++ app4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace app4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData =Console.ReadLine();
+            int numar=Convert.ToInt32(inputData);
+            if ((numar / 1000) % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+            if ((numar / 100) % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+            if ((numar / 10) % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+            if ((numar % 2) % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+
+        }
+    }
+}
