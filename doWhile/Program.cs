13451213ad865a3aa90c4a96b61a8974236f25c0
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-06 17:18:45 +0200

    Added new app.

---
+++ doWhile/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace doWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int sum=0;
+           int count = 0;
+           string inputData=Console.ReadLine();
+
+           while (inputData != "x"){
+                int numar=Convert.ToInt32(inputData);
+                sum +=numar;
+                count++;
+                inputData=Console.ReadLine();
+            };
+            if (count > 0)
+            {
+                Console.WriteLine((float) sum / count);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+
+
+            
+              
+        
+          
+
+
+
+
+
+    
+
+            
+        }
+    }
+}
