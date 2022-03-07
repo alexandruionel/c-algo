8b84f3391eb7abc1cb76d61353d3d6c34bd44cee
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-07 18:46:15 +0200

    added odd and even numbers count

---
+++ odd-even-numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OddAndEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int requiredOddAndEvenNumbers = Convert.ToInt32(Console.ReadLine());
+            int oddNumbersCount = 0;
+            int evenNumbersCount = 0;
+            while (oddNumbersCount < requiredOddAndEvenNumbers || evenNumbersCount < requiredOddAndEvenNumbers)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (a % 2 == 0)
+                {
+                    evenNumbersCount++;
+                    
+                }
+                else if (a != 0)
+                {
+                    oddNumbersCount++;
+                    
+                }
+                
+            }
+            Console.WriteLine(evenNumbersCount + " " + oddNumbersCount);
+
+        }
+    }
+}
