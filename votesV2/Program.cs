7ee489bb7fc7a414dc39ee27da1e42cc85458672
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-07 16:09:01 +0200

    Adeed program votes

---
+++ votesV2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Votes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int votesResult = 0;
+            int votesCount = 0;
+            string vote = Console.ReadLine();
+
+            while (vote != "stop")
+            {
+                votesCount++;
+
+                if (vote == "up")
+                {
+                    votesResult++;
+                }
+                else
+                {
+                    votesResult--;               }
+
+                
+                vote = Console.ReadLine();
+            }
+            Console.WriteLine(votesResult + " " + votesCount);
+
+            Console.ReadLine();
+        }
+    }
+}
+
