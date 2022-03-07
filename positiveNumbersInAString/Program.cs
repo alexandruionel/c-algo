ce608c2d3fb8707a502bcd308ca067e84e468320
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-07 14:13:35 +0200

    added a program that return the number of positie numbers in a string

---
+++ positiveNumbersInAString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace positiveNumbersInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData=Console.ReadLine();
+            int count=0;
+            while (inputData != "x")
+            {
+                int numar=Convert.ToInt32(inputData);
+                if (numar > 0)
+                {
+                    count++;
+                }
+                inputData = Console.ReadLine();
+                
+              
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
