98d4bfebe09322f77adb7f4cdf345fff7345333b
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-07 13:49:46 +0200

    A added a program that calculate factorial n.

---
+++ n!app/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace n_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData=Console.ReadLine();
+            int numar=Convert.ToInt32(inputData);
+            int factorial = 1, k;
+            
+            if (numar > 1)
+            {
+                for ( k = 2; k <= numar; k++)
+                {
+                    factorial = factorial * k;
+                                   };
+               
+                
+
+                Console.WriteLine(factorial);
+            }
+            else
+            {
+                Console.WriteLine(factorial);
+            }
+        }
+    }
+}
