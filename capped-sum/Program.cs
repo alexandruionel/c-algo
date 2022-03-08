50ac95b48bb9f15c0b8f75909e57286f1bd86345
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-08 08:33:22 +0200

    .

---
+++ capped-sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace CappedSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumLimit = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int count = 0;
+
+            while (sum <= sumLimit)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (sum+number > sumLimit)
+                {
+                    break;
+                }
+                sum += number;
+                count++;
+                
+
+            }
+            
+            Console.WriteLine(sum+ " " + count);
+           
+        }
+    }
+}
