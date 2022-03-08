33af0bd55cdcb067d43796a15aa15f316300fff5
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-08 21:36:16 +0200

    added console app that split a string in words by space, removing empty spaces

---
+++ strings2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace strings2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            var array = s.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
