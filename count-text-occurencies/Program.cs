4c913d86adb40e4a10d5642056bc77a9a554df21
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-09 08:46:23 +0200

    app that counts how many time does a substring appear in a string

---
+++ count-text-occurencies/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CountTextOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string textToFind = Console.ReadLine();
+
+            int count = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                bool match = true;
+                for (int j = 0; j < textToFind.Length; j++)
+                {
+                    if (text[i + j] != textToFind[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+            Console.Read();
+        }
+    }
+}
