1fdc458cd22c28afa95f9d9a9e5b60e6235e3db1
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-09 14:17:34 +0200

    added c.app that outputs the positive number from a string.

---
+++ strings3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _35 
+{
+    class Program
+    {
+        static void Main()
+        {
+            string rezultat="";
+            string input=Console.ReadLine();
+            while (input != "x")
+            {int numar=Convert.ToInt32(input);
+                if (numar >0)              
+                {
+                    
+                       rezultat= rezultat+Convert.ToString(numar);
+                       rezultat=rezultat+"\n";
+                        
+                    
+                }
+               input= Console.ReadLine();
+            }
+            if(rezultat == "")
+            {
+                Console.WriteLine("N/A");
+            }
+            else
+            {
+                Console.WriteLine(rezultat);
+            }
+            
+        }
+    }
+}
