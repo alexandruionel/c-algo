ea61e9b59b6ab1323c6250fef6ed0b4d70fab96e
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-10 15:53:37 +0200

    added app that reverse a string numbers separated by space.

---
+++ strings4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace V1
+{
+    class Program
+    {
+        static void Main()
+        {
+                   
+            
+            string text = Console.ReadLine();
+            string[] sep=text.Split(' ');
+            string reverse=string.Empty;
+            for (int i = sep.Length - 1; i >= 0; i--)
+            {
+                reverse += sep[i]+' ';
+            }
+            Console.Write (reverse);
+            
+                        
+
+            
+
+            
+            
+            
+            
+        }
+    }
+}
