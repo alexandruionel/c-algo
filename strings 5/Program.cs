962b36c3241c6669d21d01e7e31ab46ab1d39fdc
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-10 17:36:32 +0200

    .

---
+++ strings 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string input= Console.ReadLine();
+            string total=string.Empty;
+            int count = 1;
+            while(count<6)
+            {
+                total += input;
+                count++;
+                input = Console.ReadLine();
+            }
+           Console.WriteLine(total);
+            
+            
+
+
+
+
+
+        }
+    }
+}
