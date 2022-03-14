667153dd6abeea8ced389f27f84451a33b8a3bfb
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-14 13:33:58 +0200

    updated strings 5

--- strings 5/Program.cs
+++ strings 5/Program.cs
@@ -8,39 +8,48 @@ namespace strings_5
         static void Main(string[] args)
 
         {
-            string input= Console.ReadLine();
-            
-            string[] cuvinte=input.Split(' ');
+           
+           string totalElevi=string.Empty;
+           for(int i=0; i<6; i++)
+            {
+               totalElevi =totalElevi+Console.ReadLine() +" ";
+                
+                
+            }
+           
+           
+           string[] echipe=totalElevi.Split(' ');
+           
+           string nume =Console.ReadLine();
 
-            int count = 1;
-            while(count<=6)
+            int k=0;
+
+            while (k < echipe.Length)
             {
                 
-                count++;
-                string nume=cuvinte+" ";
-                input = Console.ReadLine();
-            }
-           
-            Console.WriteLine(nume.Length);
-           
-           for(int i = 0; i < nume.Length; i++)
-            {
-               if(nume[i] == input && i<=3)
+                if (k<3 && echipe[k]==nume)
             {
                 Console.WriteLine("echipa verde");
-            }else
+                    break;
+                }
+               
+                        
+                 else if (k>=3&& echipe[k]==nume)
             {
                 Console.WriteLine("echipa rosie");
+                    break;
+                }
+                else if(echipe[0]!=nume&&echipe[1]!=nume&&echipe[2]!=nume&&echipe[3]!=nume&&echipe[4]!=nume&&echipe[5]!=nume)
+                {
+                    Console.WriteLine("N/A");
+                    break;
+                    
+                }
+                k++;
             }
-            }
-           
-           
             
             
-
-
-
-
+   
 
         }
     }
