2db8d22f26446f04e6ccf39cc2e255cdaf616442
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-30 22:15:30 +0300

    function that outputs cmmm of 2 numbers,.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+            int x=a, y=b;
+            while (x != y)
+            {
+                if (x > y)
+                {
+                    x = x - y;
+                }
+                else
+                {
+                    y = y - x;
+                }
+            }
+            Console.WriteLine((a*b)/x);
+        }
+    }
+}
