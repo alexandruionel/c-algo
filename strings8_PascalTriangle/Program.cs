155dabb8d256f20ad7e300df07f1d94e5b264971
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-03-30 19:39:35 +0300

    count the number of simmilar elements form the beggining and ending of a string

---
+++ strings8_PascalTriangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace strings8_PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+          
+            
+            string text = Console.ReadLine();
+            string sep=text;
+            string reverse=string.Empty;
+            int count=0, i=0;int j=1;
+            
+           
+            while(i< sep.Length)
+            {
+                int numar=sep.Length-j;
+                if(sep[i] == sep[numar])
+                {
+                    count++;    
+               }
+                else
+                {
+                    break;
+                }
+                j++;
+                i++;
+            }
+            Console.WriteLine(count);
+          }  
+    }
+}
