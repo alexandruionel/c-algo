831ce7af116ddb6abc96f8dbcdb1a345b202e5d9
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-04-04 16:04:03 +0300

    added functii 7

---
+++ functii 7 calcul factorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace functii_7_calcul_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a =Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a - b;
+            int x= fak(ref a);
+            int y= fak(ref b);
+            int z= fak(ref c);
+            Console.WriteLine(x/(y*z));
+
+        }
+
+        public static int fak(ref int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i =number-1; i >0; i--)
+                number= number * i;
+                    
+                
+                
+            }
+            return number;
+
+        }
+
+    }
+}
