1ebeeeece888ef5e426429564eadb25cc6e40531
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-04-13 11:13:00 +0300

    added anagrams update

--- anagrams/Program.cs
+++ anagrams/Program.cs
@@ -7,42 +7,65 @@ namespace functii_7_calcul_factorial
 
 
         static void Main(string[] args)
-        {
-            
-            string option = Console.ReadLine();
-            
-            int n= option.Length;
-            int k=n;
-            Console.WriteLine(aranjamente(n,k));
-            
-           
-            
-
-        }
-       
-    
-            //Combination method
-            static double aranjamente(int n, int k)
-            {
-                double factn = factorial(n);
-           
-                double factnk = factorial(n-k);
-                double combination = factn / factnk;
-                return combination;
-            }
-
-            //Factorial method
-            static double factorial(int n)
-            {
-                double factorial = 1;
-                for(int i = 1; i <= n; i++)
-                {
-                    factorial *= i;
+{
+                    string inputData = Console.ReadLine();
+                    Console.WriteLine(Solution(inputData));
                 }
-                return factorial;
-            }
-    }
-    }
+
+
+                static char[] convert(string text) // converting string to char[]
+                {
+                    char[] newString = new char[text.Length];
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        newString[i] = text[i];
+                    }
+                    return newString;
+                }
+
+                static double CalculateFctorial(double number) // calc. factorial
+                {
+                    double factorial = 1;
+                    for (int i = 1; i <= number; i++)
+                        factorial *= i;
+                    return factorial;
+                }
+
+                static int CountOcc(string text, char zet) // counting occurrences
+                {                     
+                    int count = 0;
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (text[i] == zet)
+                        {
+                            count++;
+                        }
+                    }
+                    return count;
+                }
+
+                static double Solution(string text) // the soluton, counting occurences of
+                                                    // each char in string and each factorial
+                {
+                    double net = 0;
+                    double sol = 1;
+                    char[] newText = convert(text);
+                    for (int i = 0; i <= newText.Length; i++)
+                    {
+                        if (text.Length > 0)
+                        {
+                            net = CountOcc(text, newText[i]);
+                            sol = sol * CalculateFctorial(net);
+
+                            if (newText[i] == text[0])
+                            { text = text.Trim(text[0]); }
+                        }
+                    }
+
+                    double solution = CalculateFctorial(newText.Length) / sol;
+                    return solution;
+                }
+    }}
 
     
     
