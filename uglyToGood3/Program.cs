4d3b7d7ca385eeee68061c9a7c87a506885413d4
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-04-14 19:36:34 +0300

    added new ex

---
+++ uglyToGood3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FromGibberish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(TranslateFromGibberish(text));
+            Console.Read();
+        }
+
+        private static string TranslateFromGibberish(string text)
+        {
+            string translatedText = "";
+            int i = 0;
+            while (i < text.Length)
+            {
+                translatedText += text[i];
+                
+                i = IsVowel(text[i]) ? i + 1 : i + 3;
+            }
+            return translatedText;
+        }
+
+        private static bool IsVowel(char c)
+        {
+            return "bcdfghjklmnpqrstvwxyzBCDFGJKLMNPQSTVXZHRWY,.? !".IndexOf(c) != -1;
+        }
+    }
+}
