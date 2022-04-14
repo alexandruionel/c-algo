0ab77ba55c5a1f76892e2480907b20dfe2448704
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2022-04-14 19:51:12 +0300

    added new ex

---
+++ CountOfChar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CharsCountUgly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] chs = new char[0];
+            for (int i = 0; i < s.Length; i++)
+            {
+                int k = -1;
+                for (int j = 0; j < chs.Length; j++)
+                {
+                    if (chs[j] == s[i])
+                    {
+                        k = j;
+                    }
+                }
+
+                if (k == -1)
+                {
+                    Array.Resize(ref chs, chs.Length+1);
+                    chs[chs.Length - 1] = s[i];
+                }
+            }
+
+            int[] chsC = new int[chs.Length];
+
+            for (int i = 0; i < chs.Length; i++)
+            {
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (chs[i] == s[j])
+                    {
+                        chsC[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < chs.Length; i++)
+            {
+                Console.WriteLine(chs[i] + " " + chsC[i]);
+            }
+
+            Console.Read();
+        }
+    }
+}
